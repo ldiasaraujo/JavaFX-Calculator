91208a7037a73968f693fa1987d592cbad6cec7b
Lucas Dias Araujo <lucasd152001@gmail.com>
2024-01-04 22:34:14 -0300

    Adicionada função de acumular os dígitos.

--- src/edu/calc/javafx/FXMLController.java
+++ src/edu/calc/javafx/FXMLController.java
@@ -3,6 +3,9 @@ package edu.calc.javafx;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
@@ -12,13 +15,42 @@ import java.util.ResourceBundle;
 public class FXMLController implements Initializable {
 
     @FXML
-    private TextField textField;
+    private Label textField, tf2;
+
+    @FXML
+    private Button num1, num2, num3, num4, num5, num6, num7, num8, num9, num0, equals, sum, decrease, divide, multiply;
+
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         textField.setText("0");
+    }
 
+    @FXML
+    private void numberClicked (ActionEvent event){
+        String prevalue = textField.getText();
+        String number = ((Labeled)event.getSource()).getText();
+        if(prevalue.equals("0")){
+            textField.setText(number);
+        }else {
+            textField.setText(textField.getText() + number);
+        }
+    }
+
+    @FXML
+    private void operation (ActionEvent event){
+        String opt = ((Labeled)event.getSource()).getText();
+        textField.setText(textField.getText() + opt);
+    }
+
+    @FXML
+    private void calculate (ActionEvent event){
+        tf2.setText(textField.getText() + " =");
+        if(textField.getText().contains("+")){
+
+        }
 
     }
+
 }
